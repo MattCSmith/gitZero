a0ad182139117e982febfff270d971633bd3ce1d
MattCSmith <mrmcchow@gmail.com>
2020-06-28 21:11:36 +0100

    added webhook to env

--- functions/discord-webhook.function.js
+++ functions/discord-webhook.function.js
@@ -4,23 +4,20 @@ module.exports = async (data) => {
     const { title, desc, color } = data;
 
     axios
-        .post(
-            "https://discordapp.com/api/webhooks/719734752319373383/7qLEXYgd9w42ejbST4efbm1nKbSukpqf19tL3ze17rEywc9maVgZ--yYrqioqBQUGt5D",
-            {
-                crossdomain: true,
-                embeds: [
-                    {
-                        title: title.toUpperCase(),
-                        description: desc,
-                        color: color,
-                        thumbnail: {
-                            url:
-                                "https://github.githubassets.com/images/modules/logos_page/Octocat.png",
-                        },
+        .post(process.env.DISCORD_WEBHOOK, {
+            crossdomain: true,
+            embeds: [
+                {
+                    title: title.toUpperCase(),
+                    description: desc,
+                    color: color,
+                    thumbnail: {
+                        url:
+                            "https://github.githubassets.com/images/modules/logos_page/Octocat.png",
                     },
-                ],
-            }
-        )
+                },
+            ],
+        })
         .then((result) => {
             console.log(`SUCCESSFULLY SENT DISCORD WEBHOOK | "${title}"`);
         })
