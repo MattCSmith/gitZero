d44923e70427c2a04b2819b168f9fbcb18e3342c
MattCSmith <mrmcchow@gmail.com>
2020-06-28 22:20:46 +0100

    UPdated conflict message

--- languages/conflict.js
+++ languages/conflict.js
@@ -1,9 +1,10 @@
 module.exports = async (context) => {
-    console.log(context);
+    console.log(context.payload.pull_request.user.login);
+    const userName = context.payload.pull_request.user.login;
     const issueComment = context.issue({
         body:
             "## ⚠️ **MERGE CONFLICT DETECTED!** \n" +
-            `A possible conflict has been detected, you will need to resolve this before your pull request can be merged. The most common reason conflicts occur, is when the contributor does not run \`git pull origin master\` before pushing their new changes. \n\nBefore we can merge the code, you will need to resolve the conflict, there are tons of guides on Google and Youtube to help you out. If you get stuck ask over on Discord.`,
+            `@${userName} A possible conflict has been detected, you will need to resolve this before your pull request can be merged. The most common reason conflicts occur, is when the contributor does not run \`git pull origin master\` before pushing their new changes. \n\nBefore we can merge the code, you will need to resolve the conflict, there are tons of guides on Google and Youtube to help you out. If you get stuck ask over on Discord.`,
     });
 
     return context.github.issues.createComment(issueComment);
